55a022c18affbf0347bbaf1bc66c0695e016a569
Tony Tsai (PACTERA TECHNOLOGIES INC) <v-tots@microsoft.com>
2017-12-01 09:50:39 -0800

    Code for AddTwoNumbers

---
+++ twonums/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Solution
+    {
+        public Solution()
+        {
+            ;
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] retIndex;
+
+            Dictionary<int, int> myDict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                myDict[nums[i]] = i;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int foundIt = target - nums[i];
+                if(foundIt > 0)
+                {
+                    if (myDict.ContainsKey(foundIt) && myDict[foundIt] != i)
+                    {
+                        retIndex = new int[2];
+                        if(i > myDict[foundIt])
+                        {
+                            retIndex[0] = myDict[foundIt];
+                            retIndex[1] = i;
+                        }
+                        else
+                        {
+                            retIndex[0] = i;
+                            retIndex[1] = myDict[foundIt];
+                        }
+                        return retIndex;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ twonums/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ConsoleApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution solution = new Solution();
+
+            int[] myTest = {1, 2, 3, 4, 5};
+            int[] myResult;
+
+            myResult = solution.TwoSum(myTest, 8);
+
+            Assert.AreEqual(myResult[0], 2);
+            Assert.AreEqual(myResult[1], 4);
+
+            myResult = solution.TwoSum(myTest, 9);
+
+            Assert.AreEqual(myResult[0], 3);
+            Assert.AreEqual(myResult[1], 4);
+
+            myResult = solution.TwoSum(myTest, 10);
+
+            Assert.AreEqual(myResult, null);
+        }
+    }
+}
