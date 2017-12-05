2d8e93ff57c55f49844d555e0588f5b34dcfa6cb
Tony Tsai (PACTERA TECHNOLOGIES INC) <v-tots@microsoft.com>
2017-12-05 13:46:53 -0800

    Add new Question

--- FindMedianSortedArrays/FindMedianSortedArrays/Class1.cs
+++ FindMedianSortedArrays/FindMedianSortedArrays/Class1.cs
@@ -12,6 +12,20 @@ namespace LeetCode
         {
             int l1, l1data, r1, r1data, m1, m1data;
             int l2, l2data, r2, r2data, m2, m2data;
+            int mp1, mp2;
+
+            int dataLength = nums1.Length + nums2.Length;
+
+            if (dataLength % 2)
+            {
+                mp1 = (dataLength + 1) / 2;
+                mp2 = 0;
+            }
+            else
+            {
+                mp1 = dataLength  / 2;
+                mp2 = mp1 + 1;
+            }
         }
     }
 }
---
+++ RegularExpressionMatching/RegularExpressionMatching/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public Solution()
+        {
+            ;
+        }
+
+        public bool IsMatch(string s, string p)
+        {
+            int indexS = s.Length - 1;
+            int indexP = p.Length - 1;
+
+            while(indexS > 0 && indexP > 0)
+            {
+                char currentS = s[indexS++];
+                char currentP = p[indexP++];
+                int repeatS = 0;
+                int repeatP = 0;
+
+                if(currentS == '*')
+                {
+                    repeatS = Int32.MaxValue;
+                    currentS = s[indexS++];
+                    while(currentS == '*')
+                        currentS = s[indexS++];
+                }
+                else
+                {
+                    if (currentS == '.')
+                    {
+                        repeatS = 1;
+                        currentS = s[indexS++];
+                        while (currentS == '.')
+                            currentS = s[indexS++];
+                    }
+                }
+
+                if (currentP == '*')
+                {
+                    repeatP = Int32.MaxValue;
+                    currentP = s[indexS++];
+                    while (currentP == '*')
+                        currentP = s[indexP++];
+                }
+                else
+                {
+                    if (currentP == '.')
+                    {
+                        repeatP = 1;
+                        currentP = s[indexP++];
+                        while (currentP == '.')
+                            currentP = s[indexP++];
+                    }
+                }
+
+                if(currentP != currentS)
+                {
+                    if(repeatP == Int32.MaxValue)
+                    {
+                        currentP = s[indexP++];
+
+
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
