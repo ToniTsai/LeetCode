ab591bb16acee89f41e400ec8571fa65251d5181
Tony Tsai (PACTERA TECHNOLOGIES INC) <v-tots@microsoft.com>
2017-12-06 14:18:19 -0800

    Regular expression incremental implementation

--- RegularExpressionMatching/RegularExpressionMatching/Class1.cs
+++ RegularExpressionMatching/RegularExpressionMatching/Class1.cs
@@ -11,49 +11,54 @@ namespace LeetCode
 
         public bool IsMatch(string s, string p)
         {
+            if (s == null || p == null)
+                return false;
+
             int indexS = s.Length - 1;
             int indexP = p.Length - 1;
 
-            while(indexS > 0 && indexP > 0)
+
+            char currentS = s[indexS--];
+            char currentP = p[indexP--];
+
+            while (indexS > 0 && indexP > 0)
             {
-                char currentS = s[indexS++];
-                char currentP = p[indexP++];
                 int repeatS = 0;
                 int repeatP = 0;
 
                 if(currentS == '*')
                 {
                     repeatS = Int32.MaxValue;
-                    currentS = s[indexS++];
-                    while(currentS == '*')
-                        currentS = s[indexS++];
+                    currentS = s[indexS--];
+                    while (currentS == '*')
+                        currentS = s[indexS--];
                 }
                 else
                 {
                     if (currentS == '.')
                     {
                         repeatS = 1;
-                        currentS = s[indexS++];
+                        currentS = s[indexS--];
                         while (currentS == '.')
-                            currentS = s[indexS++];
+                            currentS = s[indexS--];
                     }
                 }
 
                 if (currentP == '*')
                 {
                     repeatP = Int32.MaxValue;
-                    currentP = s[indexS++];
+                    currentP = s[indexP--];
                     while (currentP == '*')
-                        currentP = s[indexP++];
+                        currentP = s[indexP--];
                 }
                 else
                 {
                     if (currentP == '.')
                     {
                         repeatP = 1;
-                        currentP = s[indexP++];
+                        currentP = s[indexP--];
                         while (currentP == '.')
-                            currentP = s[indexP++];
+                            currentP = s[indexP--];
                     }
                 }
 
@@ -61,10 +66,13 @@ namespace LeetCode
                 {
                     if(repeatP == Int32.MaxValue)
                     {
-                        currentP = s[indexP++];
-
-
+                        currentP = s[indexP--];
+                        continue;
                     }
+                }
+                else
+                {
+                    continue;
                 }
             }
 
